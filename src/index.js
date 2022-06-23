8400a56e2fbf9bde808822bac1631e359307b0af
Chkhikvadzeg <giorgichxikvadze972@gmail.com>
2022-06-23 23:12:48 +0400

    commit on 'main'

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector(".hamburger");
 const headerNav = document.querySelector(".header-nav");
 
+
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     headerNav.classList.toggle("active");
@@ -9,5 +10,4 @@ hamburger.addEventListener("click", () => {
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     headerNav.classList.remove("active");
-}))
-
+}));
