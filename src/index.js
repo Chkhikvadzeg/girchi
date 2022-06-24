b80b3634058fb9cf233f84e4a90ee0dcd1860329
Chkhikvadzeg <giorgichxikvadze972@gmail.com>
2022-06-24 14:37:29 +0400

    Changed dropdown menu

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const hamburger = document.querySelector(".hamburger");
 const headerNav = document.querySelector(".header-nav");
 
+const dropdown = document.querySelectorAll(".dropdown");
+const dropdownImage = document.querySelectorAll(".dropdown-img");
+const dropdownContent = document.querySelector(".dropdown-content");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
@@ -11,3 +14,20 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
     hamburger.classList.remove("active");
     headerNav.classList.remove("active");
 }));
+
+dropdownImage.forEach(n => n.addEventListener("click", () => {
+    n.classList.toggle("active");
+    n.parentElement.parentElement.classList.toggle("active");
+}));
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    n.parentElement.parentElement.classList.remove("active");
+}))
+
+// if(dropdown.forEach(e => e.classList.contains("active"))){
+//     document.querySelector("body").addEventListener("click", () => {
+//         dropdown.forEach(e => e.classList.remove("active"));
+//     })
+// }
+
+
